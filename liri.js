df42580ae8f7e0eaaf162e7f410ccaa23bcbabc3
drunnals313 <davidrunnals@gmail.com>
2018-05-16 22:09:03 -0400

    updated keys and js and env and ignore file

--- liri.js
+++ liri.js
@@ -21,18 +21,28 @@ var client = new Twitter(keys.twitter);
     * `do-what-it-says` */
 
 
-   var Twitter = require('twitter');
- 
-   var client = new Twitter({
-     consumer_key: '',
-     consumer_secret: '',
-     access_token_key: '',
-     access_token_secret: ''
-   });
-    
-   var params = {screen_name: 'nodejs'};
-   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-     if (!error) {
-       console.log(tweets);
-     }
-   });
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  TWITTER_CONSUMER_KEY= "SWshNFKhS289Sr0ei2teSJM3y",
+  TWITTER_CONSUMER_SECRET="l9UHKZTnY6TMxoE1Xn4dkA9wmShz62f71GcbBXdJB17UDZYDCF",
+  TWITTER_ACCESS_TOKEN_KEY= "996206395421790208-8jDkyanhbgH3mpnNPIlU69djAzZebLK",
+  TWITTER_ACCESS_TOKEN_SECRET= "tvnEt3r7nGeAgHMtf7DGt0Q8e8p9lbeHVrREZAuiqc8mj"
+});
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+
+var spotify = new Twitter({
+  Client_ID = "4918133e2a2f46cf80a3e7be1d92e99f",
+  Client_Secret = "dc81e55ec6064637a97b4711af88824d"
+});
+
+//Client ID = 4918133e2a2f46cf80a3e7be1d92e99f
+
+//Client Secret = dc81e55ec6064637a97b4711af88824d
