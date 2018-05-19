0c11e0f2090afd1f049f3579c13507135eddc0ac
drunnals313 <davidrunnals@gmail.com>
2018-05-18 21:45:19 -0400

    updated twitter get call and still getting error but at least accepting correct input

--- liri.js
+++ liri.js
@@ -1,44 +1,83 @@
 var dotenv = require("dotenv").config();
 var request = require("request");
 var Twitter = require('twitter');
-//var Spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 var keys = require("./keys");
 
 
 var client = new Twitter(keys.twitter);
-//var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 var args = process.argv;
-var inputVar = ""; 
 
 
-/* var client = new Twitter({
-  TWITTER_CONSUMER_KEY= "SWshNFKhS289Sr0ei2teSJM3y",
-  TWITTER_CONSUMER_SECRET="l9UHKZTnY6TMxoE1Xn4dkA9wmShz62f71GcbBXdJB17UDZYDCF",
-  TWITTER_ACCESS_TOKEN_KEY= "996206395421790208-8jDkyanhbgH3mpnNPIlU69djAzZebLK",
-  TWITTER_ACCESS_TOKEN_SECRET= "tvnEt3r7nGeAgHMtf7DGt0Q8e8p9lbeHVrREZAuiqc8mj"
-}); */
+
+
 
 
 function myTweets() {
-  var params = {screen_name: 'dave_hopi', count: 20, extended: true};
+  var params = {screen_name: 'dave_hopi', count: 20};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
-      if (!error) {
-        console.log(tweets);
-      } else {
+      if (error) {
         console.log('Error occurred: ' + error);
+      } //else {
+        console.log(tweets);
+        //console.log(response);
+        for (var i = 0; i < tweets.length; i++) {          
+          console.log(`------------------------------`);
+          console.log(`Dave_Hopi Tweet #${i + 1}`);
+          console.log(`Heard at: ${tweets[i].created_at}`);  //"created_at": "Sun Apr 03 23:48:36 +0000 2011", 
+          console.log(`${tweets[i].text}`);                  //"text": "RT @PostGradProblem: In preparation for the NFL lockout,...
+          console.log(`------------------------------`);
       }
+    //}
     });
 };
 
-myTweets();
+function OMDBSearch(args){
 
+}
+
+function spotifySearch(args) {
+
+}
+
+function readRandom() {
+
+}
+
+function initiate(args) {
+  switch (args[2]) {
+      case "my-tweets":
+          myTweets();
+          break;
+  /*  case "movie-this":
+          OMDBSearch(args);
+          break; 
+      case "spotify-this-song":
+          spotifySearch(args);
+          break;
+      case "do-what-it-says":
+          readRandom();
+          break; */
+      default:
+          console.log("Sorry, wrong input");
+  }
+}
+
+initiate(args);
  
-/* var spotify = new Spotify({
-  id: "4918133e2a2f46cf80a3e7be1d92e99f",
-  secret: "dc81e55ec6064637a97b4711af88824d"
-});
- 
+
+
+
+
+
+
+
+
+
+
+ /*
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
@@ -48,13 +87,5 @@ console.log(data);
 });
  */
 
-
-
-
-//Client ID = 4918133e2a2f46cf80a3e7be1d92e99f
-
-//Client Secret = dc81e55ec6064637a97b4711af88824d
-
-//http://www.omdbapi.com/?i=tt3896198&apikey=8192bdca
 
 
