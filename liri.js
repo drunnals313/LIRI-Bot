34517593141a359781b5af515ce7f22deed92658
drunnals313 <davidrunnals@gmail.com>
2018-05-18 23:21:00 -0400

    updated omdb api function

--- liri.js
+++ liri.js
@@ -1,18 +1,25 @@
-var dotenv = require("dotenv").config();
+var env = require("dotenv").config();
 var request = require("request");
 var Twitter = require('twitter');
-var Spotify = require('node-spotify-api');
-var keys = require("./keys");
-
+//var Spotify = require('node-spotify-api');
+var inquirer = require("inquirer");
+var keys = require("./keys.js");    //should this be ./keys only?
+var fs = require('fs');   // do I need this to read the random.txt file later?
 
 var client = new Twitter(keys.twitter);
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
 
 var args = process.argv;
 
-
-
-
+var searchM = '';
+/* inquirer.prompt([
+  {
+      name: "choose_command",
+      message: "Choose a LIRI command to run.",
+      type: "list",
+      choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
+  }
+]).then function */
 
 
 function myTweets() {
@@ -33,9 +40,38 @@ function myTweets() {
     //}
     });
 };
-
-function OMDBSearch(args){
-
+/* 
+ function OMDBSearch(args){
+  if (!args[3]) {
+        searchM = "Mr Nobody";
+    } else {
+        for (var i = 3; i < input.length; i++) {
+            if (i > 3 && i < input.length) {
+                searchM = searchM + "+" + input[i];
+            } else {
+                searchM += input[i];
+            }
+        }
+    }
+    var queryUrl =
+        "http://www.omdbapi.com/?t=" +
+        searchM +
+        "&y=&plot=short&apikey=9fe10b9b-1053-4bbb-9e13-156d4debb7c0";  //should I be storing my API key in a secure or private location?
+    request(queryUrl, function (err, response, body) {
+        if (err) {
+            return console.log(`Error occurred: ${err}`);
+        }
+        console.log(`Movie Title: ${JSON.parse(body).Title}`);
+        console.log(`Release Year: ${JSON.parse(body).Year}`);
+        console.log(`${JSON.parse(body).Ratings[0].Source} Rating: ${
+        JSON.parse(body).Ratings[0].Value}`);
+        console.log(`${JSON.parse(body).Ratings[1].Source} Rating: ${
+        JSON.parse(body).Ratings[1].Value}`);
+        console.log(`Country Movie was Produced: ${JSON.parse(body).Country}`);
+        console.log(`Language: ${JSON.parse(body).Language}`);
+        console.log(`Actors: ${JSON.parse(body).Actors}`);
+        console.log(`Plot: ${JSON.parse(body).Plot}`);
+    });
 }
 
 function spotifySearch(args) {
@@ -43,15 +79,20 @@ function spotifySearch(args) {
 }
 
 function readRandom() {
-
-}
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (err) {
+        return console.log(`Error occurred: ${error}`);
+    }
+    console.log(`Executing: ${data}`);
+})
+};  */
 
 function initiate(args) {
   switch (args[2]) {
       case "my-tweets":
           myTweets();
           break;
-  /*  case "movie-this":
+/*       case "movie-this":
           OMDBSearch(args);
           break; 
       case "spotify-this-song":
@@ -59,7 +100,7 @@ function initiate(args) {
           break;
       case "do-what-it-says":
           readRandom();
-          break; */
+          break;  */
       default:
           console.log("Sorry, wrong input");
   }
